ebfd6dcfcabbe1ba5994553f103b539b28dbaf05
Xuan Nguyen <john.apple9592@gmail.com>
2025-03-26 19:53:38 +1100

    fix issue

--- src/routes/(marketing)/pricing/+page.svelte
+++ src/routes/(marketing)/pricing/+page.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<!-- <script lang="ts">
   import PricingModule from "./pricing_module.svelte"
   import { WebsiteName } from "./../../../config"
 
@@ -61,59 +61,7 @@
 
   <div class="w-full my-8">
     <PricingModule callToAction="Get Started" highlightedPlanId="pro" />
-    <!-- <h1 class="text-2xl font-bold text-center mt-24">Pricing FAQ</h1>
-    <div class="flex place-content-center">
-      <div class="join join-vertical max-w-xl py-6 mx-auto">
-        <div class="collapse collapse-arrow join-item border border-primary">
-          <input type="radio" name="faq-accordion" />
-          <div class="collapse-title text-lg font-medium">
-            Is this template free to use?
-          </div>
-          <div class="collapse-content">
-            <p>Yup! This template is free to use for any project.</p>
-          </div>
-        </div>
-        <div class="collapse collapse-arrow join-item border border-primary">
-          <input type="radio" name="faq-accordion" />
-          <div class="collapse-title text-lg font-medium">
-            Why does a free template have a pricing page?
-          </div>
-          <div class="collapse-content">
-            <p>
-              The pricing page is part of the boilerplate. It shows how the
-              pricing page integrates into the billing portal and the Stripe
-              Checkout flows.
-            </p>
-          </div>
-        </div>
-        <div class="collapse collapse-arrow join-item border border-primary">
-          <input type="radio" name="faq-accordion" />
-          <div class="collapse-title text-lg font-medium">
-            What license is the template under?
-          </div>
-          <div class="collapse-content">
-            <p>The template is under the MIT license.</p>
-          </div>
-        </div>
-        <div class="collapse collapse-arrow join-item border border-primary">
-          <input type="radio" name="faq-accordion" />
-          <div class="collapse-title text-lg font-medium">
-            Can I try out purchase flows without real a credit card?
-          </div>
-          <div class="collapse-content">
-            <p>
-              Our demo page <a href="https://saasstarter.work" class="link"
-                >SaasStarter.work</a
-              > has a functional demo page, using Stripe's test environment.
-            </p>
-            <p class="mt-4">
-              You can use the credit card number 4242 4242 4242 4242 with any
-              future expiry date to test the payment and upgrade flows.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div> -->
+
 
     <svg style="display:none" version="2.0">
       <defs>
@@ -252,4 +200,6 @@
       </table>
     </div>
   </div>
-</div>
+</div> -->
+
+<h1>pricing plans coming soon</h1>
